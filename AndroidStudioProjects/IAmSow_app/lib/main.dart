fecd6c5c94b0f67e1e638bd6e2c102c14fc1fc77
Patrick Quilty <pquilty79@gmail.com>
2022-01-26 13:30:26 -0600

    added oinks

--- AndroidStudioProjects/IAmSow_app/lib/main.dart
+++ AndroidStudioProjects/IAmSow_app/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(IAmSowApp());
+
+class IAmSowApp extends StatelessWidget {
+  const IAmSowApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.pink[700],
@@ -10,12 +15,21 @@ void main() {
           title: const Text("I Am Sow"),
           backgroundColor: Colors.pink[100],
         ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/maxresdefault.jpeg'),
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              child: const Image(
+                image: AssetImage('images/maxresdefault.jpeg'),
+                alignment: Alignment.center,
+              ),
+              onPressed: () {
+                AudioCache player = AudioCache();
+                player.play('449577__zachrau__pigs-oinking.wav');
+              },
+            ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
